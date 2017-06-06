91bb337b93cdd21615b1c27472de8ec7ffd3d5e3
justinmcnary <justinmcnary@yahoo.com>
2017-06-06 11:46:26 -0400

    added Travis

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ let reportRoutes = require('./routes/reports'),
 //Secure route for variables
 require('dotenv').config({ path: 'variables.env' });
 
-mongoose.connect('mongodb://localhost/inc_rep');
+// mongoose.connect('mongodb://localhost/inc_rep'); //local
+mongoose.connect('mongodb://mcnasty:blackhawks!@ds111882.mlab.com:11882/increp'); //Mlab
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -50,7 +51,48 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log('Incident Report server is running on port 3000');
-});
+// app.listen(process.env.PORT || 3000, function() {
+//   console.log('Incident Report server is running on port 3000');
+// });
 
+let server;
+
+// this function starts our server and returns a Promise.
+// In our test code, we need a way of asynchronously starting
+// our server, since we'll be dealing with promises there.
+function runServer() {
+  const port = process.env.PORT || 3000;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Incident Report is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+// like `runServer`, this function also needs to return a promise.
+// `server.close` does not return a promise on its own, so we manually
+// create one.
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+// if server.js is called directly (aka, with `node app.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
